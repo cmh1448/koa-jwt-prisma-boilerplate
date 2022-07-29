b168e8aa5adea335b611ab999c82f8481e57aa5d
cmh1448 <cmh1448@naver.com>
2022-07-29 14:56:54 +0900

    fix type error

--- src/system/exception/exception.ts
+++ src/system/exception/exception.ts
@@ -1,11 +1,11 @@
 import { Context, Next } from 'koa';
 import logger from '../log/logger';
 
-export interface ExeptionInterface extends Error {
+export interface ExceptionInterface extends Error {
   code: number;
 }
 
-export class Exeption extends Error implements ExeptionInterface {
+export class Exception extends Error implements ExceptionInterface {
   constructor(msg: string, code: number) {
     super(msg);
     this.code = code;
@@ -14,7 +14,7 @@ export class Exeption extends Error implements ExeptionInterface {
   code: number;
 }
 
-export async function exeptionHandler(ctx: Context, next: Next) {
+export async function exceptionHandler(ctx: Context, next: Next) {
   try {
     await next();
   } catch (ex: any) {
