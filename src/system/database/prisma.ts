ba842f23a8a1014c67183adfe42a3a1d38d4732a
cmh1448 <cmh1448@naver.com>
2022-07-29 14:41:35 +0900

    add DB Setup

--- src/system/database/prisma.ts
+++ src/system/database/prisma.ts
@@ -1,9 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import logger from "../log/logger";
 
 const prisma = new PrismaClient();
-
-prisma.$connect()
-  .then(() => logger.info("PRISMA DB CONNECTED"));
 
 export default prisma;
